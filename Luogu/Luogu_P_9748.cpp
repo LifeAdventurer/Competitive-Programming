91de23c69758b2985f676c78c4cd7e9586c5f35c
Moon <108756201+LifeAdventurer@users.noreply.github.com>
2023-10-23 14:49:33 +0800

    Create Luogu_P_9748.cpp

---
+++ Luogu/Luogu_P_9748.cpp
@@ -0,0 +1,44 @@
+/**              _         ___                       _
+ *              | |    _  /  _|                     | |                        _
+ *              | |   |_| | |  _____     ____    ___| |__    __ _____  _____  | |   _   _  ____  _____  ____
+ *    author:   | |    _ [   ]/  _  \   /  _ \  /  _  |\ \  / //  _  \|  _  \[   ] | | | ||  __|/  _  \|  __|
+ *              | L__ | | | | | ____|   | |_\ \ | |_| | \ \/ / | ____|| | | | | |_ | |_| || /   | ____|| /
+ *              L____||_| |_| \_____|   \____\_\\_____/  \__/  \_____||_| |_| |___|\_____/|_|   \_____||_|
+ *
+ *    Life is an adventure. Be adventurous.
+ *
+ *    created: 23.10.2023 11:35:32
+ *
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  int n;
+  cin >> n;
+  int cnt = 1, ans = 0;
+  while(n > 0){
+    if(!ans && n % 3 == 1) ans = cnt;
+    n -= (n + 2) / 3;
+    cnt++;
+  }
+  cout << cnt - 1 << " " << ans << '\n';
+
+  return 0;
+}
+
+/**
+ *        "To stop trying is never the solution."
+ *        "You are not alone. Lord have your way."
+ *        "Failure is another blessing. Only by accepting it calmly can it bring growth."
+**/

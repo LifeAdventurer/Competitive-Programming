a3ef36586a95802c3276180e3baa044439af3375
Moon <108756201+LifeAdventurer@users.noreply.github.com>
2023-11-07 14:15:14 +0800

    Create P_1_The_Triangle.cpp

---
+++ IOI/1994/P_1_The_Triangle.cpp
@@ -0,0 +1,53 @@
+/**              _         ___                       _
+ *              | |    _  /  _|                     | |                        _
+ *              | |   |_| | |  _____     ____    ___| |__    __ _____  _____  | |   _   _  ____  _____  ____
+ *    author:   | |    _ [   ]/  _  \   /  _ \  /  _  |\ \  / //  _  \|  _  \[   ] | | | ||  __|/  _  \|  __|
+ *              | L__ | | | | | ____|   | |_\ \ | |_| | \ \/ / | ____|| | | | | |_ | |_| || /   | ____|| /
+ *              L____||_| |_| \_____|   \____\_\\_____/  \__/  \_____||_| |_| |___|\_____/|_|   \_____||_|
+ *
+ *    Life is an adventure. Be adventurous.
+ *
+ *    created: 06.09.2023 21:40:50
+ *
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  int n;
+  cin >> n;
+  vector<vector<int>> dp(n);
+  for(int i = 0; i < n; ++i){
+    for(int j = 0; j < i + 1; ++j){
+      int x;
+      cin >> x;
+      dp[i].push_back(x);
+    }
+  }
+
+  for(int i = n - 1; i > 0; --i){
+    for(int j = 0; j < i; ++j){
+      dp[i - 1][j] += max(dp[i][j], dp[i][j + 1]);
+    }
+  }
+
+  cout << dp[0][0] << '\n';
+
+  return 0;
+}
+
+/**
+ *        "To stop trying is never the solution."
+ *        "You are not alone. Lord have your way." Smiling<3 & Sabrina<3
+ *        "Failure is another blessing. Only by accepting it calmly can it bring growth."
+**/

6485ea52c18e270919d8aeb0d15307e3986cec08
lifeadventurer <life0adventurer@gmail.com>
2023-11-22 23:32:22 +0800

    backup competitive programming workspace from the local - IOI folder

---
+++ IOI/2000/Luogu_P_1435.cpp
@@ -0,0 +1,48 @@
+/**              _         ___                       _
+ *              | |    _  /  _|                     | |                        _
+ *              | |   |_| | |  _____     ____    ___| |__    __ _____  _____  | |   _   _  ____  _____  ____
+ *    author:   | |    _ [   ]/  _  \   /  _ \  /  _  |\ \  / //  _  \|  _  \[   ] | | | ||  __|/  _  \|  __|
+ *              | L__ | | | | | ____|   | |_\ \ | |_| | \ \/ / | ____|| | | | | |_ | |_| || /   | ____|| /
+ *              L____||_| |_| \_____|   \____\_\\_____/  \__/  \_____||_| |_| |___|\_____/|_|   \_____||_|
+ *
+ *    Life is an adventure. Be adventurous.
+ *
+ *    created: 17.09.2023 22:39:21
+ *
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  string s;
+  cin >> s;
+  int n = s.size();
+  string t = s;
+  reverse(t.begin(), t.end());
+  vector<vector<int>> dp(n + 1, vector<int>(n + 1));
+  for(int i = 1; i <= n; ++i){
+    for(int j = 1; j <= n; ++j){
+      if(s[i - 1] == t[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+      else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+    }
+  }
+  cout << n - dp[n][n] << '\n';
+
+  return 0;
+}
+
+/**
+ *        "To stop trying is never the solution."
+ *        "You are not alone. Lord have your way." Smiling<3 & Sabrina<3
+ *        "Failure is another blessing. Only by accepting it calmly can it bring growth."
+**/

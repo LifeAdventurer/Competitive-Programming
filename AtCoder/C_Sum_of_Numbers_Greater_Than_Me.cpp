c3fafd6f7a09e16fdae217ff6f4ebc179240fb82
lifeadventurer <life0adventurer@gmail.com>
2023-12-02 22:03:46 +0800

    ABC 331 contest codes

---
+++ AtCoder/C_Sum_of_Numbers_Greater_Than_Me.cpp
@@ -0,0 +1,55 @@
+/**              _         ___                       _
+ *              | |    _  /  _|                     | |                        _
+ *              | |   |_| | |  _____     ____    ___| |__    __ _____  _____  | |   _   _  ____  _____  ____
+ *    author:   | |    _ [   ]/  _  \   /  _ \  /  _  |\ \  / //  _  \|  _  \[   ] | | | ||  __|/  _  \|  __|
+ *              | L__ | | | | | ____|   | |_\ \ | |_| | \ \/ / | ____|| | | | | |_ | |_| || /   | ____|| /
+ *              L____||_| |_| \_____|   \____\_\\_____/  \__/  \_____||_| |_| |___|\_____/|_|   \_____||_|
+ *
+ *    Life is an adventure. Be adventurous.
+ *
+ *    created: 02.12.2023 20:06:06
+ *
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  map<int, int> m;
+  for(int i = 0; i < n; ++i){
+    cin >> v[i];
+    m[v[i]]++;
+  }
+
+  vector<long long> pre;
+  int cnt = 0;  
+  for(auto [x, y] : m){
+    pre.push_back(1LL * x * y);
+    m[x] = cnt++;
+  }
+  partial_sum(pre.begin(), pre.end(), pre.begin());
+
+  for(int i = 0; i < n; ++i){
+    cout << pre.back() - pre[m[v[i]]] << " \n"[i == n - 1];
+  }
+
+  return 0;
+}
+
+/**
+ *        "To stop trying is never the solution."
+ *        "You are not alone. Lord have your way."
+ *        "Failure is another blessing. Only by accepting it calmly can it bring growth."
+**/

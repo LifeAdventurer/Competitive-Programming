4e75dbc4631cb88e0c20e5f7ef81c716ae0b4b29
lifeadventurer <life0adventurer@gmail.com>
2023-12-07 22:59:16 +0800

    CP progress - 7 Dec 2023

---
+++ Luogu_P_1855.cpp
@@ -0,0 +1,48 @@
+/**              _         ___                       _
+ *              | |    _  /  _|                     | |                        _
+ *              | |   |_| | |  _____     ____    ___| |__    __ _____  _____  | |   _   _  ____  _____  ____
+ *    author:   | |    _ [   ]/  _  \   /  _ \  /  _  |\ \  / //  _  \|  _  \[   ] | | | ||  __|/  _  \|  __|
+ *              | L__ | | | | | ____|   | |_\ \ | |_| | \ \/ / | ____|| | | | | |_ | |_| || /   | ____|| /
+ *              L____||_| |_| \_____|   \____\_\\_____/  \__/  \_____||_| |_| |___|\_____/|_|   \_____||_|
+ *
+ *    Life is an adventure. Be adventurous.
+ *
+ *    created: 07.12.2023 21:44:43
+ *
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n, M, T;
+  cin >> n >> M >> T;
+  vector<vector<int>> dp(M + 1, vector<int>(T + 1));
+  for(int i = 0; i < n; ++i){
+    int m, t;
+    cin >> m >> t;
+    for(int i = M; i >= m; --i){
+      for(int j = T; j >= t; --j){
+        dp[i][j] = max(dp[i][j], dp[i - m][j - t] + 1);
+      }
+    }
+  }
+  cout << dp[M][T] << '\n';
+
+  return 0;
+}
+
+/**
+ *        "To stop trying is never the solution."
+ *        "You are not alone. Lord have your way."
+ *        "Failure is another blessing. Only by accepting it calmly can it bring growth."
+**/
